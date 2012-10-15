73571f1d9d3d71e4bc2a3f41865a01c11a4708ad
N Harvey <nharvey@mymail.mines.edu>
2012-10-15 15:53:41 -0600

    consolidating once again

--- src/com/example/android_fling/GestureFunActivity.java
+++ src/com/example/android_fling/GestureFunActivity.java
@@ -12,6 +12,8 @@ import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
 import android.widget.FrameLayout;
 
 public class GestureFunActivity extends Activity {
@@ -58,5 +60,76 @@ public class GestureFunActivity extends Activity {
     	    translate.postTranslate(dx, dy);  
     	    invalidate();  
     	}  
+    	
+    	public void resetLocation(){
+    		translate=new Matrix();//go back to identity matrix
+    		invalidate();
+    	}
     }
+    public class GestureListener implements OnGestureListener, OnDoubleTapListener {
+    	PlayAreaView view;
+    	public GestureListener(PlayAreaView playAreaView) {
+    		this.view=playAreaView;
+    	}
+
+    	@Override
+    	public boolean onDoubleTap(MotionEvent arg0) {
+    		Log.v(DEBUG_TAG, "onDoubleTap");  
+    	    view.resetLocation();  
+    	    return true;  
+    	}
+
+    	@Override
+    	public boolean onDoubleTapEvent(MotionEvent arg0) {
+    		// TODO Auto-generated method stub
+    		return false;
+    	}
+
+    	@Override
+    	public boolean onSingleTapConfirmed(MotionEvent arg0) {
+    		// TODO Auto-generated method stub
+    		return false;
+    	}
+
+    	@Override
+    	public boolean onDown(MotionEvent arg0) {
+    		Log.v(DEBUG_TAG, "onDown");  
+    		return true; 
+    	}
+
+    	@Override
+    	public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2,
+    			float arg3) {
+    		//view.move(, dy)
+    		return true;
+    	}
+
+    	@Override
+    	public void onLongPress(MotionEvent arg0) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    	@Override
+    	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float distanceX,
+    			float distanceY) {
+    			Log.v(DEBUG_TAG, "onScroll");  
+    			view.move(-distanceX, -distanceY);
+    			return true;  
+    		}
+
+    		@Override
+    		public void onShowPress(MotionEvent arg0) {
+    			// TODO Auto-generated method stub
+
+    		}
+
+    		@Override
+    		public boolean onSingleTapUp(MotionEvent arg0) {
+    			// TODO Auto-generated method stub
+    			return false;
+    		}
+
+    	}
+
 }
